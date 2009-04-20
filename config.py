263a4af3079fd738ac60f15bf6ea721bf2ae6994
Matt Jones <matt@triplehelix.org>
2009-04-19 23:16:16 -0700

    explicit path

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ engine_params = {'echo': False}
 twitter_user = "nuance"
 github_user = "nuance"
 yelp_user = '5UeW6xMmWClWcOrw6cPXXg'
-recently_read_file = "RECENTLY-READ"
+recently_read_file = "/srv/www/domains/roaringtofu.org/RECENTLY-READ"
 
 ga_key = "2393143-1"
