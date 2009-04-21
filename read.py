c8373072f5b4c7a05575bdda2bea20ccd0d7ed25
Matt Jones <matt-biz@mhjones.org>
2009-04-20 23:53:33 -0700

    add bookmarklet

--- read.py
+++ read.py
@@ -5,7 +5,8 @@ from private import post_key
 from util import render_mako
 
 urls = ('/save', 'save_post',
-		'/add/(.+)', 'add_post')
+		'/add/(.+)', 'add_post',
+		'/bookmarklet/(.+)', 'bookmarklet')
 app_read = web.application(urls, globals())
 
 class save_post(object):
@@ -25,7 +26,7 @@ class save_post(object):
 		meta.session.add(article)
 		meta.session.commit()
 
-		web.redirect('/add/%s' % input.key)
+		web.redirect(url)
 
 
 class add_post(object):
@@ -38,3 +39,16 @@ class add_post(object):
 
 		return render_mako('article_submission', key=key)
 
+class bookmarklet(object):
+	"""
+	Blog servlet
+	"""
+	def GET(self, key):
+		if key == 'css':
+			web.header("Content-Type","text/css; charset=utf-8")
+			return render_mako('bookmarklet_css')
+		if key != post_key:
+			return app_read.notfound()
+
+		web.header("Content-Type","application/javascript; charset=utf-8")
+		return render_mako('bookmarklet', key=key)
