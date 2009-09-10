4b60a436df25b94c06ad08b3137b871c08527fc3
mattj <mattj@sff7u163.706.yelpcorp.com>
2009-09-10 16:26:56 -0700

    oops

--- yelp_redir.py
+++ yelp_redir.py
@@ -6,7 +6,7 @@ class yelp_redir(object):
 		decoded_query = unquote(query)
 		if '@' in decoded_query:
 			desc, loc = decoded_query.split('@', 1)
-			search = 'find_desc=%s&find_loc=%s' % (quote(search.strip()), quote(location.strip()))
+			search = 'find_desc=%s&find_loc=%s' % (quote(desc.strip()), quote(location.strip()))
 		else:
 			search = 'find_desc=%s' % quote(decoded_query.strip())
 
